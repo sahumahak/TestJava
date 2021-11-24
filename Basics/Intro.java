7cd474bdaf6e963d08722e6e5f016ccdb4ec47c3
sahumahak <sahumahak803@gmail.com>
2021-11-24 14:26:24 +0530

    1st commit

---
+++ Basics/Intro.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class Intro {
+	public static void main (String[] args) {
+		System.out.println("Hii");
+		System.out.println("Hii my name is Mahak");
+		System.out.println("I am a 2nd year student");
+		System.out.println("Thankyou");
+		
+	}
+		
+	}
+
---
+++ Basics/Print2.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class Print2 {
+	public static void main (String[] args) {
+		System.out.println("Java is a programming language");
+		System.out.println("Java is OOP");
+		System.out.println("Java is Object Oriented Programming");
+	}
+
+}
